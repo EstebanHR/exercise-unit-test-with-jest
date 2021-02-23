60d35c28ec321da5ae33dcf74c82814995291b9c
EstebanHR <estebanhr1981@hotmail.com>
2021-02-23 03:21:41 +0000

    Proyecto Final

--- app.test.js
+++ app.test.js
@@ -4,6 +4,11 @@ expect(total).toBe(5329.17)
 })
 
 test ('50 Euros debe ser al cambio 60', ()=>{
-    let total = convierte(50,"USD");
-expect(total).toBe(5329.17)
+    let total = convierte(50,"EUR");
+expect(total).toBe(60)
+})
+
+test ('50 Yenes debe ser al cambio 0.03', ()=>{
+    let total = convierte(50,"JPY");
+expect(total).toBe(0.03)
 })
