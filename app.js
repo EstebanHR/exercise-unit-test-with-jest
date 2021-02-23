0555b54b5801a78850115b97b34e33fa765e0f63
EstebanHR <estebanhr1981@hotmail.com>
2021-02-23 03:16:20 +0000

    segundo commit

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 // this is my function that sums two numbers
-const sum = (a,b) => {
+const sum = (a, b) => {
     return a + b
 }
 
-let convert = (montoIn, monedaIn, montoOut, monedaOut) =>{
-
-    if (monedaIn=="USD"){
-        return (montoIn/1.2)*127.9
-    }else if(moneda=="EUR"){
-        return ()
-    }
-}
-
-// we declare the function with the exact name "fromEuroToDollar"
-const fromEuroToDollar = function(valueInEuro){
-    // convert the given valueInEuro to dollars
-    let valueInDollar = valueInEuro * 1.2;
-    //return the doller value
-    return valueInDollar;
-}
-// we include fromEuroToDollar here as well because it needs to be exported
-module.exports = { sum, fromEuroToDollar }
-
-
+let convierte = (monto, moneda) =>{
+    if (moneda=="USD") {
+        return parseFloat(((monto/1.2)*127.9).toFixed(2))
+    } else if(moneda=="EUR") {
+        return parseFloat((monto*1.2).toFixed(2))
+    } else if (moneda=="JPY") {
+        return parseFloat((monto/127.9)* 0.8).toFixed(2)) }
+          
 // just a console log for ourselves.
-console.log (sum(7,3))
+console.log(sum(7, 3))
+console.log(convierte(50, "USD"))
+console.log(convierte(50, "EUR"))
+console.log(convierte(50, "JPY"))
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+module.exports = { sum, convierte };
---
+++ app.test.js
@@ -0,0 +1,9 @@
+test ('50 dolares debe ser al cambio 5329.17', ()=>{
+    let total = convierte(50,"USD");
+expect(total).toBe(5329.17)
+})
+
+test ('50 Euros debe ser al cambio 60', ()=>{
+    let total = convierte(50,"USD");
+expect(total).toBe(5329.17)
+})
